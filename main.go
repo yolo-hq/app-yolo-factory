bf3fc6eafd1364447b1a0d93f7e7d8b070bd37eb
Jomon Johnson <jomon.entero@gmail.com>
2026-03-29 20:20:09 +0530

    refactor: update repo form layout, reorder registry registrations, and relocate status badge template

--- main.go
+++ main.go
@@ -16,12 +16,9 @@ import (
 
 func main() {
 	// Entities
-	registry.Register(entities.Question{}, entities.Repo{}, entities.Run{}, entities.Task{})
+	registry.Register(entities.Repo{}, entities.Run{}, entities.Task{}, entities.Question{})
 
 	// Repositories
-	registry.RegisterRepoFactory("Question", func(db any) (any, any) {
-		return bunrepo.NewReadRepository[entities.Question](db.(*bun.DB)), bunrepo.NewWriteRepository[entities.Question](db.(*bun.DB))
-	})
 	registry.RegisterRepoFactory("Repo", func(db any) (any, any) {
 		return bunrepo.NewReadRepository[entities.Repo](db.(*bun.DB)), bunrepo.NewWriteRepository[entities.Repo](db.(*bun.DB))
 	})
@@ -31,6 +28,9 @@ func main() {
 	registry.RegisterRepoFactory("Task", func(db any) (any, any) {
 		return bunrepo.NewReadRepository[entities.Task](db.(*bun.DB)), bunrepo.NewWriteRepository[entities.Task](db.(*bun.DB))
 	})
+	registry.RegisterRepoFactory("Question", func(db any) (any, any) {
+		return bunrepo.NewReadRepository[entities.Question](db.(*bun.DB)), bunrepo.NewWriteRepository[entities.Question](db.(*bun.DB))
+	})
 
 	// Filters
 	registry.RegisterFilter("Question", filters.QuestionFilter{})
@@ -39,9 +39,9 @@ func main() {
 
 	// Actions
 	registry.RegisterActions("Question", &actions.CreateQuestionAction{}, &actions.ResolveQuestionAction{})
-	registry.RegisterActions("Run", &actions.CompleteRunAction{}, &actions.CreateRunAction{})
-	registry.RegisterActions("Task", &actions.CreateTaskAction{}, &actions.ExecuteTaskAction{}, &actions.CancelTaskAction{}, &actions.UpdateTaskAction{})
 	registry.RegisterActions("Repo", &actions.CreateRepoAction{}, &actions.UpdateRepoAction{})
+	registry.RegisterActions("Run", &actions.CompleteRunAction{}, &actions.CreateRunAction{})
+	registry.RegisterActions("Task", &actions.CancelTaskAction{}, &actions.CreateTaskAction{}, &actions.ExecuteTaskAction{}, &actions.UpdateTaskAction{})
 
 	// Commands
 	command.Register(&commands.CleanupRuns{})
